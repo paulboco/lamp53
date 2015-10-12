7ed16df110d25aa628c2ec577914b23b2b9c3fa2
Paulbo Co <paulboco@cableone.net>
2015-10-12 09:20:37 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -24,7 +24,7 @@ sudo apt-get install git vim apache2 mysql-server libapache2-mod-auth-mysql php5
 sudo mysql_install_db
 sudo /usr/bin/mysql_secure_installation
 sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
-sudo apt-get install php5-cgi php5-cli php5-curl php5-common php5-gd php5-mysql
+sudo apt-get install php5-cgi php5-cli php5-curl php5-common php5-gd php5-mysql php5-xdebug
 
 # Install phpMyAdmin
 mkdir /home/vagrant/Code
@@ -33,12 +33,19 @@ git clone https://github.com/paulboco/phpmyadmin-insecure.git phpmyadmin
 cd /etc/apache2/sites-available
 sudo cp default phpmyadmin
 sudo sed -i -e 's|/var/www|/vagrant/public|g' default
+sudo sed -i -e 's|AllowOverride None|AllowOverride All|g' default
 sudo sed -i -e 's|/var/www|/home/vagrant/Code/phpmyadmin|g' phpmyadmin
 sudo sed -i -e "s|@localhost|@localhost\n\tServerName phpmyadmin.53|g" phpmyadmin
 sudo a2ensite phpmyadmin
 
-# Display PHP Errors
+# Enable Mods
+sudo a2enmod rewrite
+
+# Edit php.ini
 sudo sed -i -e 's|display_errors = Off|display_errors = On|g' /etc/php5/apache2/php.ini
+sudo sed -i -e 's|html_errors = Off|html_errors = On|g' /etc/php5/apache2/php.ini
+echo "\n# Zend Extensions" | sudo tee -a /etc/php5/apache2/php.ini
+echo 'zend_extension="/usr/lib/php5/20090626+lfs/xdebug.so"' | sudo tee -a /etc/php5/apache2/php.ini
 
 # Set Apache2 Default Host Name
 echo "ServerName localhost" | sudo tee /etc/apache2/httpd.conf
@@ -47,8 +54,15 @@ echo "ServerName localhost" | sudo tee /etc/apache2/httpd.conf
 sudo service apache2 restart
 
 # Clean up
+echo "------------------------------------------------------------------------"
 echo "Cleaning up. Please wait..."
+echo "------------------------------------------------------------------------"
 sudo apt-get clean
 sudo dd if=/dev/zero of=/EMPTY bs=1M
 sudo rm -f /EMPTY
-cat /dev/null > ~/.bash_history && history -c && exit
+cat /dev/null > ~/.bash_history && history -c
+
+# Done
+echo "------------------------------------------------------------------------"
+echo "End lamp53 script"
+echo "------------------------------------------------------------------------"
